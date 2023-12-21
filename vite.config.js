80050c548600c502ecf709f12accfdd66656bbd0
Sohail <sayedsohail150@gmail.com>
2023-12-21 12:43:59 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:"/keeper-app/"
+  base:"/keeper-app/",
+  plugins: [react()]
 })
